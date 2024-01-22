dbb7c897265613c07be2f76c14e5316ea6fd9186
PUGAZHENTHI THANIARASU I R <105981927+pugazhenthi2002@users.noreply.github.com>
2024-01-22 14:54:27 +0530

    Add files via upload

---
+++ CircularProgressBar.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsDraft
+{
+    class CircularProgressBar: UserControl
+    {
+        private int outerRadius = 0, innerCircleLocation = 0, percentage = 0, loadingCount=0;
+        private float bendAngle = 0, animationBendAngle = 0;
+        public bool isCompleted = false;
+        public Timer CircularProgressTimer = new Timer();
+        public Timer LoadingAnimationTimer = new Timer();
+
+        public CircularProgressBar()
+        {
+            CircularProgressTimer.Interval = 60;
+            CircularProgressTimer.Tick += OnPercentageChange;
+
+            LoadingAnimationTimer.Interval = 30;
+            LoadingAnimationTimer.Tick += OnLoadingAnimation;
+        }
+
+        private void OnLoadingAnimation(object sender, EventArgs e)
+        {
+            for (int ctr = 0; ctr <= percentage; ctr++)
+            {
+                loadingCount = ctr;
+                this.Invalidate();
+            }
+        }
+
+        private void OnPercentageChange(object sender, EventArgs e)
+        {
+            percentage++;
+            Invalidate();
+
+            if (percentage == 100)
+            {
+                isCompleted = true;
+                CircularProgressTimer.Stop();
+                LoadingAnimationTimer.Stop();
+            }
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            bendAngle = percentage * 360 / 100;
+            animationBendAngle = loadingCount * 360 / 100;
+            outerRadius = Height;
+            innerCircleLocation = outerRadius / 10;
+
+
+            DoubleBuffered = true;
+            Color color = Color.FromArgb(115, Color.Gray);
+            Color animationColor = Color.FromArgb(50, Color.White);
+            Brush innerBrush = new SolidBrush(Color.White);
+            Brush outerBrush = new SolidBrush(color);
+            Font f = new Font(new FontFamily("Arial"), 20);
+            StringFormat s = new StringFormat();
+            s.Alignment = StringAlignment.Center;
+            s.LineAlignment = StringAlignment.Center;
+
+            Pen animationPen = new Pen(animationColor, innerCircleLocation);
+            animationPen.StartCap = animationPen.EndCap = LineCap.Round;
+
+            Pen pen = new Pen(Color.Green, innerCircleLocation);
+            pen.StartCap = pen.EndCap = LineCap.Round;
+
+            Graphics g = e.Graphics;
+            g.SmoothingMode = SmoothingMode.AntiAlias;
+
+            g.FillEllipse(outerBrush, 0, 0, outerRadius, outerRadius);  //outerCircle
+            g.FillEllipse(innerBrush, innerCircleLocation, innerCircleLocation, outerRadius - (innerCircleLocation * 2), outerRadius - (innerCircleLocation * 2));  //innerCircle
+            g.DrawArc(pen, innerCircleLocation / 2, innerCircleLocation / 2, outerRadius - innerCircleLocation, outerRadius - innerCircleLocation, 270, bendAngle);
+            g.DrawArc(animationPen, innerCircleLocation / 2, innerCircleLocation / 2, outerRadius - innerCircleLocation, outerRadius - innerCircleLocation, 270, animationBendAngle);
+
+            g.DrawString(percentage + "%", f, new SolidBrush(Color.Red), new RectangleF(0, 0, Width, Height), s);
+
+            
+
+            innerBrush.Dispose();   outerBrush.Dispose();   f.Dispose();    s.Dispose();    pen.Dispose();
+        }
+
+        protected override void OnResize(EventArgs e)
+        {
+            Width = Height = Math.Max(Width, Height);
+            Margin = new Padding(0);
+        }
+    }
+}
---
+++ RippleButton.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsDraft
+{
+    class RippleButton: Button
+    {
+        private int rippleSize;
+        private List<Ripple> Clicks= new List<Ripple>();
+        private List<Timer> Timers= new List<Timer>();
+        private Timer t = new Timer();
+        private int Count = 0;
+        public RippleButton()
+        {
+            this.MouseDown += RippleButton_MouseDown;
+            this.Paint += RippleButton_Paint;
+            
+        }
+
+        private void RippleButton_MouseDown(object sender, MouseEventArgs e)
+        {
+            Clicks.Add(new WindowsFormsDraft.Ripple { X = e.X, Y = e.Y, Size = 0 });
+
+           if(Count==0)
+            {
+                Count++;
+                t = new Timer();
+                t.Interval = 30;
+                t.Tick += RippleTimer_Tick;
+                t.Start();
+            }
+            
+        }
+
+        private void RippleTimer_Tick(object sender, EventArgs e)
+        {
+            for (int Iter = 0; Iter < Clicks.Count; Iter++)
+            {
+                Clicks[Iter].Size += 5;
+            }
+
+            this.Invalidate();
+            this.DoubleBuffered = true;
+
+            for (int Iter = 0; Iter < Clicks.Count; Iter++)
+            {
+
+                if (Clicks[Iter].Size > (Width + Height))
+                {
+                    if (Clicks.Count == 1)
+                    {
+                        t.Stop();
+                        Count = 0;
+                    }
+                    rippleSize = Clicks[Iter].Size = 0;
+                    Clicks.RemoveAt(Iter);
+                }
+            }
+                
+        }
+
+        private void RippleButton_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            foreach (var Iter in Clicks)
+            {
+                if (Iter.Size > 0)
+                {
+                    
+                    Color nonIntersectingColor = Color.FromArgb(40, Color.AliceBlue);
+                    var b = new SolidBrush(nonIntersectingColor);
+                    g.FillEllipse(b, Iter.X - (Iter.Size), Iter.Y - (Iter.Size), 2 * Iter.Size, 2 * Iter.Size);
+                    
+                    
+                    g.SmoothingMode = SmoothingMode.AntiAlias;
+                    
+                    b.Dispose();
+                }
+            }
+            //string buttonText = "Pugazh";
+            //Font f = new Font(new FontFamily("Arial"), 20);
+            //float singleLetterPixelX = f.Size * 4 / 5;
+            //float singleLetterPixelY = f.Size * 5 / 4;
+            //g.DrawString(buttonText, f, new SolidBrush(Color.Red), new PointF((Width - (buttonText.Length * singleLetterPixelX)) / 2, (Height - singleLetterPixelY) / 2));
+
+            //g.DrawLine(new Pen(new SolidBrush(Color.Black)), new Point(0, 50), new Point(buttonText.Length*16,50));
+        }
+    }
+
+    class Ripple
+    {
+        public int X, Y, Size;
+    }
+}
