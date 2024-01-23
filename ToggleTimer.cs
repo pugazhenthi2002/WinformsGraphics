b93fff04990d61aa1341afa186710c04ac47a423
PUGAZHENTHI THANIARASU I R <105981927+pugazhenthi2002@users.noreply.github.com>
2024-01-23 14:02:14 +0530

    Add files via upload

---
+++ ToggleTimer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsDraft
+{
+    class ToggleTimer : Timer
+    {
+        public delegate void ToggleButtonMovementHandler(object sender, int movement);
+        public event ToggleButtonMovementHandler ToggleButtonMoved;
+        private int stepMovement;
+
+        public void Start(int X)
+        {
+            stepMovement = X;
+            base.Start();
+        }
+
+        protected override void OnTick(EventArgs e)
+        {
+            ToggleButtonMoved?.Invoke(this, stepMovement);
+        }
+    }
+}
