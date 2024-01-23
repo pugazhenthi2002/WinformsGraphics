405f4d938327fd6fa906974a7d6ab75a2ca0c7cd
PUGAZHENTHI THANIARASU I R <105981927+pugazhenthi2002@users.noreply.github.com>
2024-01-23 14:01:37 +0530

    Update CircularProgressBar.cs

--- CircularProgressBar.cs
+++ CircularProgressBar.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Drawing.Drawing2D;
@@ -12,7 +12,7 @@ namespace WindowsFormsDraft
     class CircularProgressBar: UserControl
     {
         private int outerRadius = 0, innerCircleLocation = 0, percentage = 0, loadingCount=0;
-        private float bendAngle = 0, animationBendAngle = 0;
+        private float bendAngle = 0, animationBendAngle = 270;
         public bool isCompleted = false;
         public Timer CircularProgressTimer = new Timer();
         public Timer LoadingAnimationTimer = new Timer();
@@ -28,10 +28,13 @@ namespace WindowsFormsDraft
 
         private void OnLoadingAnimation(object sender, EventArgs e)
         {
-            for (int ctr = 0; ctr <= percentage; ctr++)
+            if(loadingCount>percentage)
             {
-                loadingCount = ctr;
-                this.Invalidate();
+                loadingCount = 0;
+            }
+            else
+            {
+                loadingCount++;
             }
         }
 
@@ -51,23 +54,23 @@ namespace WindowsFormsDraft
         protected override void OnPaint(PaintEventArgs e)
         {
             bendAngle = percentage * 360 / 100;
-            animationBendAngle = loadingCount * 360 / 100;
+            animationBendAngle = (loadingCount * 360 / 100) + 270;
             outerRadius = Height;
             innerCircleLocation = outerRadius / 10;
 
-
             DoubleBuffered = true;
             Color color = Color.FromArgb(115, Color.Gray);
-            Color animationColor = Color.FromArgb(50, Color.White);
+
+            
+
             Brush innerBrush = new SolidBrush(Color.White);
             Brush outerBrush = new SolidBrush(color);
+
             Font f = new Font(new FontFamily("Arial"), 20);
+
             StringFormat s = new StringFormat();
             s.Alignment = StringAlignment.Center;
             s.LineAlignment = StringAlignment.Center;
-
-            Pen animationPen = new Pen(animationColor, innerCircleLocation);
-            animationPen.StartCap = animationPen.EndCap = LineCap.Round;
 
             Pen pen = new Pen(Color.Green, innerCircleLocation);
             pen.StartCap = pen.EndCap = LineCap.Round;
@@ -78,11 +81,11 @@ namespace WindowsFormsDraft
             g.FillEllipse(outerBrush, 0, 0, outerRadius, outerRadius);  //outerCircle
             g.FillEllipse(innerBrush, innerCircleLocation, innerCircleLocation, outerRadius - (innerCircleLocation * 2), outerRadius - (innerCircleLocation * 2));  //innerCircle
             g.DrawArc(pen, innerCircleLocation / 2, innerCircleLocation / 2, outerRadius - innerCircleLocation, outerRadius - innerCircleLocation, 270, bendAngle);
-            g.DrawArc(animationPen, innerCircleLocation / 2, innerCircleLocation / 2, outerRadius - innerCircleLocation, outerRadius - innerCircleLocation, 270, animationBendAngle);
+
+
+            LoadingAnimation(g);
 
             g.DrawString(percentage + "%", f, new SolidBrush(Color.Red), new RectangleF(0, 0, Width, Height), s);
-
-            
 
             innerBrush.Dispose();   outerBrush.Dispose();   f.Dispose();    s.Dispose();    pen.Dispose();
         }
@@ -92,5 +95,44 @@ namespace WindowsFormsDraft
             Width = Height = Math.Max(Width, Height);
             Margin = new Padding(0);
         }
-    }
+
+        private void LoadingAnimation(Graphics g)
+        {
+            Color animationColorL1 = Color.FromArgb(20, Color.White);
+            Color animationColorL2 = Color.FromArgb(35, Color.White);
+            Color animationColorL3 = Color.FromArgb(50, Color.White);
+            Color animationColorM = Color.FromArgb(80, Color.White);
+            Color animationColorR3 = Color.FromArgb(50, Color.White);
+            Color animationColorR2 = Color.FromArgb(35, Color.White);
+            Color animationColorR1 = Color.FromArgb(20, Color.White);
+
+            Pen animationPenLeft1 = new Pen(animationColorL1, innerCircleLocation);
+            Pen animationPenLeft2 = new Pen(animationColorL2, innerCircleLocation);
+            Pen animationPenLeft3 = new Pen(animationColorL3, innerCircleLocation);
+            Pen animationPenRight3 = new Pen(animationColorR3, innerCircleLocation);
+            Pen animationPenRight2 = new Pen(animationColorR2, innerCircleLocation);
+            Pen animationPenRight1 = new Pen(animationColorR1, innerCircleLocation);
+            Pen animationPenMiddle = new Pen(animationColorM, innerCircleLocation);
+
+            if (bendAngle > 2 && !isCompleted && animationBendAngle + 25 < bendAngle + 270)
+            {
+                g.DrawArc(animationPenLeft1, innerCircleLocation / 2, innerCircleLocation / 2, outerRadius - innerCircleLocation, outerRadius - innerCircleLocation, animationBendAngle, 3);
+                g.DrawArc(animationPenLeft2, innerCircleLocation / 2, innerCircleLocation / 2, outerRadius - innerCircleLocation, outerRadius - innerCircleLocation, animationBendAngle + 3, 3);
+                g.DrawArc(animationPenLeft3, innerCircleLocation / 2, innerCircleLocation / 2, outerRadius - innerCircleLocation, outerRadius - innerCircleLocation, animationBendAngle + 6, 4);
+                g.DrawArc(animationPenMiddle, innerCircleLocation / 2, innerCircleLocation / 2, outerRadius - innerCircleLocation, outerRadius - innerCircleLocation, animationBendAngle + 10, 5);
+                g.DrawArc(animationPenRight3, innerCircleLocation / 2, innerCircleLocation / 2, outerRadius - innerCircleLocation, outerRadius - innerCircleLocation, animationBendAngle + 15, 4);
+                g.DrawArc(animationPenRight2, innerCircleLocation / 2, innerCircleLocation / 2, outerRadius - innerCircleLocation, outerRadius - innerCircleLocation, animationBendAngle + 19, 3);
+                g.DrawArc(animationPenRight1, innerCircleLocation / 2, innerCircleLocation / 2, outerRadius - innerCircleLocation, outerRadius - innerCircleLocation, animationBendAngle + 22, 3);
+            }
+
+            animationPenLeft1.Dispose();
+            animationPenLeft2.Dispose();
+            animationPenLeft3.Dispose();
+            animationPenRight1.Dispose();
+            animationPenRight2.Dispose();
+            animationPenRight3.Dispose();
+            animationPenMiddle.Dispose();
+        }
+    }   
+
 }
